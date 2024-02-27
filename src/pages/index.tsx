7a747d1b2d0c1b42874eeaebec5f90ef21b76d8d
jiyoon <vywns9978@gmail.com>
2024-02-27 18:04:25 +0900

    [🔧 setting] eslint와 prettier 설치 및 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Head from "next/head";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -12,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main />
     </>
   );
 }
