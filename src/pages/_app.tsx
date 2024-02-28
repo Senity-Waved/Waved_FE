5a4555be0ff0690368a86eefc1d61cf7aac996ca
jiyoon <vywns9978@gmail.com>
2024-02-28 12:54:34 +0900

    [🔧 setting] recoil과 react-query 설치 및 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import theme from '@/styles/theme';
 import global from '@/styles/global';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={global} />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <Global styles={global} />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
