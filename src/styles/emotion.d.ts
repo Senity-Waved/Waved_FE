a37d19aa182ea2efcb36ffe7f14c94d3a17f4ddf
sryung <sryung1225@gmail.com>
2024-02-29 19:15:23 +0900

    [🔧 setting] 글로벌 스타일 적용

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -2,10 +2,8 @@ import '@emotion/react';
 
 declare module '@emotion/react' {
   export interface Theme {
-    color: {
-      primary: string;
-      positive: string;
-      negative: string;
+    [key: string]: {
+      [key: string]: string;
     };
   }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,27 @@
 import { Theme } from '@emotion/react';
 import color from '@/constants/color';
 
+export const calcRem = (size: number) => `${size / 16}rem`;
+
 const theme: Theme = {
   color: {
     primary: color.PRIMARY,
     positive: color.POSITIVE,
     negative: color.NEGATIVE,
+  },
+  spacing: {
+    xxxs: calcRem(2),
+    xxs: calcRem(4),
+    xs: calcRem(8),
+    sm: calcRem(12),
+    base: calcRem(16),
+    md: calcRem(20),
+    lg: calcRem(24),
+    xl: calcRem(32),
+    xxl: calcRem(40),
+    xxxl: calcRem(48),
+    56: calcRem(56),
+    60: calcRem(64),
   },
 };
 
