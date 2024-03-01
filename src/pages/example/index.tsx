fef3d23d3530645ad9777833cce2bf630edd6b24
sryung <sryung1225@gmail.com>
2024-03-02 06:44:47 +0900

    [⚙️ chore] 공통 컴포넌트 예시 페이지 생성 #11
    - 레이아웃 구조 예시 및 추후 발견되는 공통 컴포넌트에 대해서 함께 예시를 공유하기 위한 용도로 별도 페이지 분리 진행
    - TabMenu 라우팅 일부 수정

---
+++ src/pages/example/index.tsx
@@ -0,0 +1,78 @@
+import HeadMeta from '@/components/HeadMeta';
+import Header from '@/components/common/Header';
+import TabMenu from '@/components/common/TabMenu';
+import Btn from '@/components/common/Btn';
+import BottomFixedBtn from '@/components/common/BottomFixedBtn';
+import Footer from '@/components/common/Footer';
+import ModalTest from '@/components/modal/ModalTest';
+
+export default function Example() {
+  return (
+    <>
+      <HeadMeta
+        title="공통 컴포넌트 예시"
+        description="각 최상위 페이지에 지금과 같이 HeadMeta를 사용하면 됩니다. 공통 컴포넌트 예시를 모아두기 위한 페이지입니다. 배포 이전 삭제 예정입니다."
+      />
+      <Header headerText="마이 챌린지" rightText="인증패스" />
+      <main>
+        <TabMenu
+          tabs={[
+            { href: '/example', text: '메뉴1' },
+            { href: '/example#example2', text: '메뉴2' },
+            { href: '/example#example3', text: '메뉴3' },
+          ]}
+        />
+        <div id="example2">
+          <Btn
+            btns={[
+              {
+                text: '버튼1',
+                styleType: 'primary',
+                size: 'large',
+              },
+              {
+                type: 'submit',
+                text: '버튼2',
+                isDisabled: true,
+                styleType: 'gray',
+                size: 'large',
+              },
+              {
+                text: '버튼3',
+                styleType: 'white',
+                size: 'large',
+              },
+            ]}
+          />
+          <Btn
+            btns={[
+              {
+                text: '작은 버튼1',
+                styleType: 'primary',
+                size: 'small',
+              },
+              {
+                text: '작은 버튼2',
+                styleType: 'primary',
+                size: 'small',
+              },
+            ]}
+          />
+        </div>
+        <div id="example3">
+          <ModalTest />
+        </div>
+        <BottomFixedBtn
+          btns={[
+            {
+              text: '화면 하단 고정 버튼',
+              styleType: 'primary',
+              size: 'large',
+            },
+          ]}
+        />
+      </main>
+      <Footer />
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,17 @@
 import HeadMeta from '@/components/HeadMeta';
 import Header from '@/components/common/Header';
-import TabMenu from '@/components/common/TabMenu';
-import Btn from '@/components/common/Btn';
-import BottomFixedBtn from '@/components/common/BottomFixedBtn';
 import Footer from '@/components/common/Footer';
-import ModalTest from '@/components/modal/ModalTest';
 
 export default function Home() {
   return (
     <>
-      <HeadMeta title="HOME" />
-      <main>
-        <Header headerText="Title" rightText="인증패스" />
-        <ModalTest />
-        <TabMenu
-          tabs={[
-            { href: '/', text: '메뉴1' },
-            { href: '/menu2', text: '메뉴2' },
-            { href: '/menu3', text: '메뉴3' },
-          ]}
-        />
-        <Btn
-          btns={[
-            {
-              text: '버튼1',
-              styleType: 'primary',
-              size: 'large',
-            },
-            {
-              type: 'submit',
-              text: '버튼2',
-              isDisabled: true,
-              styleType: 'gray',
-              size: 'large',
-            },
-            {
-              text: '버튼3',
-              styleType: 'primary',
-              size: 'small',
-            },
-          ]}
-        />
-        <BottomFixedBtn
-          btns={[
-            {
-              text: '화면 하단 고정 버튼',
-              styleType: 'primary',
-              size: 'large',
-            },
-          ]}
-        />
-        <Footer />
+      <HeadMeta />
+      <h1 className="a11yHidden">WAVED</h1>
+      <Header headerText="Title" rightText="인증패스" />
+      <main id="home">
+        <p>Home</p>
       </main>
+      <Footer />
     </>
   );
 }
