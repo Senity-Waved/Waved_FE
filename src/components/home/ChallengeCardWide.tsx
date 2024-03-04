7e36456097b5e42e0777536b928f97cec803970a
sryung <sryung1225@gmail.com>
2024-03-05 02:18:31 +0900

    [✨ feat] 진행 중인 챌린지 구현 #14
    - 홈(챌린지 목록) 공통 스타일링
    - 넓은 챌린지 카드 컴포넌트 ChallengeCardWide 생성 후 더미 데이터 연결
    - 가로 스크롤 적용 : 스크롤 미노출 + 터치 전용 부드러운 스크롤 스타일 적용

---
+++ src/components/home/ChallengeCardWide.tsx
@@ -0,0 +1,73 @@
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface IChallengeCardWide {
+  challenge_id: number;
+  title: string;
+  thumbnail: string;
+}
+
+export default function ChallengeCardWide({
+  challenge_id,
+  thumbnail,
+  title,
+}: IChallengeCardWide) {
+  return (
+    <SChallengeCardWide>
+      <Link href={`/challenge/${challenge_id}`}>
+        <SImage>
+          <Image
+            src="https://via.placeholder.com/198x108.jpg"
+            alt={title || ''}
+            width={198}
+            height={108}
+            priority
+          />
+        </SImage>
+        <STitle>
+          <h3>{title}</h3>
+          <span>10일 차</span>
+        </STitle>
+      </Link>
+    </SChallengeCardWide>
+  );
+}
+
+const SChallengeCardWide = styled.li`
+  display: inline-block;
+  width: 198px;
+  height: 158px;
+  &:not(:last-child) {
+    margin-right: 0.75rem;
+  }
+`;
+
+const SImage = styled.div`
+  position: relative;
+  width: 100%;
+  margin-bottom: 0.5rem;
+  line-height: 0;
+`;
+
+const STitle = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  line-height: 22px;
+  h3 {
+    flex: 1;
+    color: ${({ theme }) => theme.color.gray_3c};
+    font-size: ${({ theme }) => theme.fontSize.body2};
+    font-weight: ${({ theme }) => theme.fontWeight.body2};
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+  span {
+    flex-shrink: 0;
+    color: ${({ theme }) => theme.color.gray_83};
+    font-size: ${({ theme }) => theme.fontSize.caption1};
+    font-weight: ${({ theme }) => theme.fontWeight.caption1};
+  }
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,126 @@
+import styled from '@emotion/styled';
+import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '@/components/common/Layout';
-import TopBanner from '@/components/challengeList/TopBanner';
+import TopBanner from '@/components/home/TopBanner';
+import ChallengeCardWide from '@/components/home/ChallengeCardWide';
+
+const myData = [
+  {
+    challenge_id: 34525,
+    title: '기술 면접 1기',
+    thumbnail: 'https://via.placeholder.com/198x108.jpg',
+  },
+  {
+    challenge_id: 341756,
+    title: '기술 면접 2기',
+    thumbnail: 'https://via.placeholder.com/198x108.jpg',
+  },
+  {
+    challenge_id: 69565,
+    title: '기술 면접 3기',
+    thumbnail: 'https://via.placeholder.com/198x108.jpg',
+  },
+  {
+    challenge_id: 154654,
+    title: '기술 면접 4기',
+    thumbnail: 'https://via.placeholder.com/198x108.jpg',
+  },
+];
 
 export default function Home() {
   return (
     <Layout headerText="WAVED">
       <TopBanner />
+      <section>
+        <STitle>
+          <h2>진행 중인 챌린지</h2>
+          <SLink href="/mychallenge">
+            <Image
+              src="/icons/icon-left-arrow.svg"
+              alt="마이 챌린지로 가기"
+              width={24}
+              height={24}
+              priority
+            />
+          </SLink>
+        </STitle>
+        <SListScrollX>
+          {myData.map((challenge) => (
+            <ChallengeCardWide {...challenge} />
+          ))}
+        </SListScrollX>
+      </section>
+      <section>
+        <STitle>
+          <h2>💻 프론트엔드 챌린지</h2>
+          <p>프론트엔드 개발자들을 위한 챌린지</p>
+        </STitle>
+      </section>
+      <section>
+        <STitle>
+          <h2>👨‍💻 백엔드 챌린지</h2>
+          <p>백엔드 개발자들을 위한 챌린지</p>
+        </STitle>
+      </section>
+      <section>
+        <STitle>
+          <h2>🏃‍♂️ 학습 챌린지</h2>
+          <p>확정 텍스트가 필요한 챌린지 챌린지 챌린지</p>
+        </STitle>
+      </section>
+      <section>
+        <STitle>
+          <h2>🏃‍♂️ 생활 챌린지</h2>
+          <p>확정 텍스트가 필요한 챌린지 챌린지 챌린지</p>
+        </STitle>
+      </section>
     </Layout>
   );
 }
+
+const STitle = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 92px;
+  padding: 1.5rem 1.25rem;
+  color: ${({ theme }) => theme.color.gray_3c};
+  h2 {
+    font-size: ${({ theme }) => theme.fontSize.subtitle1};
+    font-weight: ${({ theme }) => theme.fontWeight.subtitle1};
+  }
+  p {
+    font-size: ${({ theme }) => theme.fontSize.body4};
+    font-weight: ${({ theme }) => theme.fontWeight.body4};
+  }
+`;
+
+const SLink = styled(Link)`
+  position: absolute;
+  top: 50%;
+  right: 20px;
+  transform: translateY(-50%);
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  img {
+    transform: rotate(180deg);
+  }
+`;
+
+const SListScrollX = styled.ul`
+  width: 100%;
+  overflow-x: auto;
+  padding: 0 20px;
+  white-space: nowrap;
+  -webkit-overflow-scrolling: touch;
+  /* 스크롤바 미노출 */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome , Safari , Opera */
+  }
+`;
