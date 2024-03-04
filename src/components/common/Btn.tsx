1434492edf3528855f59e276ab8d77bbe393bd68
sryung <sryung1225@gmail.com>
2024-03-04 12:45:38 +0900

    [🎨 style] Btn color 스타일 오타 수정

--- src/components/common/Btn.tsx
+++ src/components/common/Btn.tsx
@@ -73,7 +73,7 @@ export const SBtn = styled.button<ISBtn>`
     background-color: ${({ styleType, theme }) =>
       ({
         primary: theme.color.dark,
-        gray: theme.color.gary_bf,
+        gray: theme.color.gray_bf,
         white: theme.color.gray_ec,
         white_line: theme.color.gray_ec,
       })[styleType]};
