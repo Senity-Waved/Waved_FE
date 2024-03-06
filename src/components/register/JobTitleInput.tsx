be240bbd3c459dbf42ae91e9208e48d397b8f789
jiyoon <vywns9978@gmail.com>
2024-03-06 12:55:01 +0900

    [🎨 style] 직군 선택 페이지 스타일링 적용 및 직군 추가 #1

--- src/components/register/JobTitleInput.tsx
+++ src/components/register/JobTitleInput.tsx
@@ -13,42 +13,96 @@ export default function JobTitleInput({
 }: IJobTitleInput) {
   return (
     <SJobTitleInputWrapper>
-      <p>해당 직군 선택</p>
-      <SFrontBtn
-        type="button"
-        jobTitle={currentJobTitle}
-        onClick={() => updateRegisterData({ jobTitle: JOBTITLE.FRONT })}
-      >
-        프론트엔드
-      </SFrontBtn>
-      <SBackBtn
-        type="button"
-        jobTitle={currentJobTitle}
-        onClick={() => updateRegisterData({ jobTitle: JOBTITLE.BACK })}
-      >
-        백엔드
-      </SBackBtn>
-      <SFullBtn
-        type="button"
-        jobTitle={currentJobTitle}
-        onClick={() => updateRegisterData({ jobTitle: JOBTITLE.FULL })}
-      >
-        풀스택
-      </SFullBtn>
-      <SDevBtn
-        type="button"
-        jobTitle={currentJobTitle}
-        onClick={() => updateRegisterData({ jobTitle: JOBTITLE.DEV })}
-      >
-        데브옵스
-      </SDevBtn>
+      <SJobTitleChipWrapper>
+        <SJobTitleChip
+          type="button"
+          active={currentJobTitle === JOBTITLE.FRONT}
+          onClick={() => updateRegisterData({ jobTitle: JOBTITLE.FRONT })}
+        >
+          프론트엔드
+        </SJobTitleChip>
+        <SJobTitleChip
+          type="button"
+          active={currentJobTitle === JOBTITLE.BACK}
+          onClick={() => updateRegisterData({ jobTitle: JOBTITLE.BACK })}
+        >
+          백엔드
+        </SJobTitleChip>
+        <SJobTitleChip
+          type="button"
+          active={currentJobTitle === JOBTITLE.FULL}
+          onClick={() => updateRegisterData({ jobTitle: JOBTITLE.FULL })}
+        >
+          풀 스택
+        </SJobTitleChip>
+        <SJobTitleChip
+          type="button"
+          active={currentJobTitle === JOBTITLE.ANDROID}
+          onClick={() => updateRegisterData({ jobTitle: JOBTITLE.ANDROID })}
+        >
+          안드로이드
+        </SJobTitleChip>
+        <SJobTitleChip
+          type="button"
+          active={currentJobTitle === JOBTITLE.IOS}
+          onClick={() => updateRegisterData({ jobTitle: JOBTITLE.IOS })}
+        >
+          IOS
+        </SJobTitleChip>
+        <SJobTitleChip
+          type="button"
+          active={currentJobTitle === JOBTITLE.DEV}
+          onClick={() => updateRegisterData({ jobTitle: JOBTITLE.DEV })}
+        >
+          데브옵스
+        </SJobTitleChip>
+        <SJobTitleChip
+          type="button"
+          active={currentJobTitle === JOBTITLE.BLOCKCHAIN}
+          onClick={() => updateRegisterData({ jobTitle: JOBTITLE.BLOCKCHAIN })}
+        >
+          블록체인
+        </SJobTitleChip>
+        <SJobTitleChip
+          type="button"
+          active={currentJobTitle === JOBTITLE.MACINE}
+          onClick={() => updateRegisterData({ jobTitle: JOBTITLE.MACINE })}
+        >
+          머신러닝
+        </SJobTitleChip>
+        <SJobTitleChip
+          type="button"
+          active={currentJobTitle === JOBTITLE.ETC}
+          onClick={() => updateRegisterData({ jobTitle: JOBTITLE.ETC })}
+        >
+          기타
+        </SJobTitleChip>
+      </SJobTitleChipWrapper>
     </SJobTitleInputWrapper>
   );
 }
 
 const SJobTitleInputWrapper = styled.div``;
 
-const SFrontBtn = styled.button<{ jobTitle: string }>``;
-const SBackBtn = styled.button<{ jobTitle: string }>``;
-const SFullBtn = styled.button<{ jobTitle: string }>``;
-const SDevBtn = styled.button<{ jobTitle: string }>``;
+const SJobTitleChipWrapper = styled.div`
+  margin: 0 1.25rem;
+  width: auto;
+  display: flex;
+  flex-flow: row wrap;
+  gap: 0.5rem;
+  margin-top: 1.5rem;
+  margin-bottom: 12.375rem;
+`;
+
+const SJobTitleChip = styled.button<{ active: boolean }>`
+  width: auto;
+  padding: 0.5rem 0.75rem;
+  height: 36px;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.color.gray_f9};
+  border: 1px solid
+    ${({ theme, active }) =>
+      active ? theme.color.normal : theme.color.gray_ec};
+  color: ${({ theme, active }) =>
+    active ? theme.color.normal : theme.color.gray_3c};
+`;
--- src/constants/jobTitle.ts
+++ src/constants/jobTitle.ts
@@ -2,7 +2,12 @@ const JOBTITLE = {
   FRONT: 'frontend',
   BACK: 'backend',
   FULL: 'fullstack',
+  IOS: 'ios',
+  ANDROID: 'android',
   DEV: 'devops',
+  BLOCKCHAIN: 'blockchain',
+  MACINE: 'machinelearning',
+  ETC: 'etc',
 };
 
 export default JOBTITLE;
