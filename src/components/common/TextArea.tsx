c21706a5d46e7f7f666a3ec415a68cbe6f97e9f0
yujin1228 <starng11@naver.com>
2024-03-07 11:06:56 +0900

    [✨ feat] textarea 글자수표시 #17

--- src/components/common/TextArea.tsx
+++ src/components/common/TextArea.tsx
@@ -1,14 +1,25 @@
 import styled from '@emotion/styled';
+import { useState } from 'react';
 
 interface ITextArea {
   placeholder: string;
 }
 
 export default function TextArea({ placeholder }: ITextArea) {
+  const [textLength, setTextLength] = useState<number>(0);
+
+  const countTextLength = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextLength(e.target.value.length);
+  };
+
   return (
     <SWrapper>
-      <STextArea placeholder={placeholder} />
-      <STextLength>&#40;7/300&#41;</STextLength>
+      <STextArea
+        placeholder={placeholder}
+        maxLength={700}
+        onChange={countTextLength}
+      />
+      <STextLength>&#40;{textLength}/700&#41;</STextLength>
     </SWrapper>
   );
 }
@@ -31,6 +42,7 @@ const STextArea = styled.textarea`
   box-shadow: 0 3px 8px 2px rgba(35, 62, 112, 0.05);
   resize: none;
   margin-bottom: 1.5rem;
+  overflow-y: auto;
 
   &::placeholder {
     color: ${({ theme }) => theme.color.gray_bf};
@@ -38,6 +50,14 @@ const STextArea = styled.textarea`
 
   &:focus {
     outline: none;
+  }
+
+  -webkit-overflow-scrolling: touch;
+  /* 스크롤바 미노출 */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome , Safari , Opera */
   }
 `;
 
