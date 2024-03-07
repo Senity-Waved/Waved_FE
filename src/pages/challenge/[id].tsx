a1fe2a7f5280fcba00eb38a9115ade3bfe0a3817
sryung <sryung1225@gmail.com>
2024-03-08 03:22:14 +0900

    [✨ feat] 챌린지 상세 정보 데이터 렌더링 대비 목업 데이터 설정 #15

--- src/pages/challenge/[id].tsx
+++ src/pages/challenge/[id].tsx
@@ -6,6 +6,65 @@ import TabMenu from '@/components/common/TabMenu';
 import BottomFixedBtn from '@/components/common/BottomFixedBtn';
 import ChallengeSummary from '@/components/challenge/ChallengeSummary';
 
+interface IChallengeReview {
+  reviewId: number;
+  author: string;
+  jobTitle?: string;
+  createdDate: string;
+  context: string;
+}
+
+interface IChallenge {
+  type: 'write' | 'photo' | 'link' | 'github';
+  title: string;
+  thumbnail: string;
+  description: string;
+  participantCount: number;
+  startDate: string;
+  endDate: string;
+  isFree: boolean;
+  reviews: IChallengeReview[];
+  verificationMethod: string;
+  verificationExample: string[];
+}
+
+const challengeData: IChallenge = {
+  type: 'photo',
+  title: '기술 면접 챌린지 1기',
+  thumbnail: 'https://via.placeholder.com/600x400.jpg',
+  description:
+    '자기 개발은 목표를 설정하고 달성하기 위한 여정입니다. 이 블로그 포스트에서는 일상 생활에 쉽게 통합할 수 있는 5가지 핵심 습관을 소개합니다. 첫 번째는 목표 설정과 시간 관리입니다. 이는 개인적 성취와 전문적 성장을 위한 기초를 마련합니다.\n두 번째 습관은 긍정적 사고를 통한 자기 격려입니다. 이는 도전을 극복하고 성공으로 나아가는 데 중요합니다. 세 번째는 건강 유지를 위한 일상적인 운동과 균형 잡힌 식단입니다. 건강한 몸은 능률적인 마음의 기초입니다. 네 번째는 지속적인 학습과 자기 계발입니다. ',
+  participantCount: 23,
+  startDate: '03월 04일 (월)',
+  endDate: '03월 15일 (금)',
+  isFree: true,
+  reviews: [
+    {
+      reviewId: 325544,
+      author: '서퍼dfk34s',
+      jobTitle: '직군',
+      createdDate: '2024년 12월 31일',
+      context:
+        '피그마 플러그인 개발에 관심이 생겨서 찾아보게 되었는데,많은 도움이 되네요.',
+    },
+    {
+      reviewId: 8535435,
+      author: '닉네임은최대열글자로',
+      jobTitle: '프론트엔드',
+      createdDate: '2023년 01월 01일',
+      context:
+        '피그마 플러그인 개발에 관심이 생겨서 찾아보게 되었는데,많은 도움이 되네요. 피그마 플러그인 개발에 관심이 생겨서 찾아보게 되었는데,많은 도움이 되네요. 피그마 플러그인 개발에 관심이 생겨서 찾아보게 되었는데,많은 도움이 되네요. 피그마 플러그인 개발에 관심이 생겨서 찾아보게 되었는데,많은 도움이 되네요.',
+    },
+  ],
+  verificationMethod:
+    '자기 개발은 목표를 설정하고 달성하기 위한 여정입니다. 이 블로그 포스트에서는 일상 생활에 쉽게 통합할 수 있는 5가지 핵심 습관을 소개합니다. 첫 번째는 목표 설정과 시간 관리입니다. 이는 개인적 성취와 전문적 성장을 위한 기초를 마련합니다.\n두 번째 습관은 긍정적 사고를 통한 자기 격려입니다. 이는 도전을 극복하고 성공으로 나아가는 데 중요합니다. 세 번째는 건강 유지를 위한 일상적인 운동과 균형 잡힌 식단입니다. 건강한 몸은 능률적인 마음의 기초입니다. 네 번째는 지속적인 학습과 자기 계발입니다. ',
+  verificationExample: [
+    'https://via.placeholder.com/150x218.jpg',
+    'https://via.placeholder.com/150x218.jpg',
+    'https://via.placeholder.com/150x218.jpg',
+  ],
+};
+
 export default function Challenge() {
   const router = useRouter();
   const id = typeof router.query.id === 'string' ? router.query.id : '';
