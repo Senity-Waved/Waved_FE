26c6b7a4e8ac5dfe90669e8a822976b347e47c51
sryung <sryung1225@gmail.com>
2024-03-09 04:51:24 +0900

    [✨ feat] meta 태그를 포함한 head 요소 누락 적용 #14

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,21 +1,36 @@
 import styled from '@emotion/styled';
 import Image from 'next/image';
+import Head from 'next/head';
 import { SHeaderWrapper } from '@/components/common/Header';
 
 export default function HomeHeader() {
   return (
-    <SHeader>
-      <SLogo>
-        <h1 className="a11yHidden">WAVED</h1>
-        <Image
-          alt="WAVED 로고"
-          src="https://via.placeholder.com/100x30.jpg"
-          width={100}
-          height={30}
+    <>
+      <Head>
+        <title>WAVED</title>
+        <meta
+          name="description"
+          content="개발 직군 취준생들을 위한 챌린지 서비스, WAVED입니다."
         />
-      </SLogo>
-      <SAlarm type="button" />
-    </SHeader>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SHeader>
+        <SLogo>
+          <h1 className="a11yHidden">WAVED</h1>
+          <Image
+            alt="WAVED 로고"
+            src="https://via.placeholder.com/100x30.jpg"
+            width={100}
+            height={30}
+          />
+        </SLogo>
+        <SAlarm type="button" />
+      </SHeader>
+    </>
   );
 }
 
