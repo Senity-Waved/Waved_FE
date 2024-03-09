a3370a1cd72e5ba8a135604b7fae177763d5770e
jiyoon <vywns9978@gmail.com>
2024-03-09 23:36:54 +0900

    [🎨 style] 프로필 수정 페이지 화면 구조 설계 및 데이터 전송 테스트  #2

---
+++ src/pages/profile/edit/index.tsx
@@ -0,0 +1,70 @@
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import styled from '@emotion/styled';
+import Layout from '@/components/common/Layout';
+import NicknameInput from '@/components/register/NicknameInput';
+import { IRegisterState } from '@/types/register';
+import JobTitleInput from '@/components/register/JobTitleInput';
+import PrivacyInput from '@/components/register/PrivacyInput';
+import Btn from '@/components/common/Btn';
+
+export default function ProfileEdit() {
+  const router = useRouter();
+  const [editProfile, setEditProfile] = useState<IRegisterState>({
+    birthYear: '',
+    gender: null,
+    nickname: '',
+    jobTitle: '',
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`프로필 수정: ${JSON.stringify(editProfile)}`);
+    router.push('/profile').catch((error) => {
+      console.error('페이지 이동에 실패하였습니다.', error);
+    });
+  };
+
+  const updateProfileData = (newProfile: Partial<IRegisterState>) => {
+    setEditProfile({ ...editProfile, ...newProfile });
+  };
+
+  return (
+    <Layout
+      noFooter
+      headerText="프로필 수정"
+      title="프로필 수정"
+      description="회원의 닉네임, 직군, 출생연도, 성별 등 개인 정보를 수정할 수 있는 페이지입니다."
+    >
+      <div>
+        <form onSubmit={handleSubmit}>
+          <NicknameInput updateData={updateProfileData} />
+          <JobTitleInput
+            jobTitle={editProfile.jobTitle}
+            updateData={updateProfileData}
+          />
+          <PrivacyInput
+            gender={editProfile.gender}
+            updateData={updateProfileData}
+          />
+          <SEditBtnWrapper>
+            <Btn
+              btns={[
+                {
+                  text: '수정하기',
+                  styleType: 'primary',
+                  size: 'large',
+                  type: 'submit',
+                },
+              ]}
+            />
+          </SEditBtnWrapper>
+        </form>
+      </div>
+    </Layout>
+  );
+}
+
+const SEditBtnWrapper = styled.div`
+  margin: 1.25rem 1.25rem 0 1.25rem;
+`;
