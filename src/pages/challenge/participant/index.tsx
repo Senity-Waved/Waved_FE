7ea31cf1411634147f930afd36e546b6fd8ee1e8
jiyoon <vywns9978@gmail.com>
2024-03-11 23:37:06 +0900

    [🎨 style] list 아이콘 추가 및 예치금 슬라이더 스타일링 적용 #4

--- src/pages/challenge/participant/index.tsx
+++ src/pages/challenge/participant/index.tsx
@@ -5,6 +5,7 @@ import BottomFixedBtn from '@/components/common/BottomFixedBtn';
 import DepositRefundGuide from '@/components/challenge/participant/DepositRefundGuide';
 import DepositGuide from '@/components/challenge/participant/DepositGuide';
 import paymentMethods from '@/constants/payment';
+import ChallengeSummary from '@/components/challenge/ChallengeSummary';
 
 export default function ChallengeParticipant() {
   const [deposit, setDeposit] = useState<number>(5000);
@@ -12,12 +13,11 @@ export default function ChallengeParticipant() {
     paymentMethods.CREDITCARD,
   );
   const depositAmounts = [5000, 10000, 20000, 25000, 30000, 50000, 100000];
-
-  const handleChange = (newDeposit: number) => {
-    const closestDeposit = depositAmounts.reduce((prev, curr) =>
-      Math.abs(curr - newDeposit) < Math.abs(prev - newDeposit) ? curr : prev,
-    );
-    setDeposit(closestDeposit);
+  const challengeData = {
+    title: '기술면접 챌린지 1기',
+    participantCount: 23,
+    startDate: '03월 04일 (월)',
+    endDate: '03월 15일 (금)',
   };
 
   return (
@@ -27,39 +27,28 @@ export default function ChallengeParticipant() {
       description="선택한 챌린지의 예치금을 설정하고, 결제를 하는 페이지입니다. 결제를 한 뒤 신청하기 버튼을 누르면 챌린지 신청이 완료됩니다."
     >
       <SChallengeParticipantWrapper>
-        <SChallengeSummary>ChallengeSummary</SChallengeSummary>
+        <ChallengeSummary
+          title={challengeData.title}
+          participantCount={challengeData.participantCount}
+          startDate={challengeData.startDate}
+          endDate={challengeData.endDate}
+          condition="recruiting"
+        />
         <DepositGuide />
         <SDepositSettingWrapper>
           <SDepositAmout>{deposit.toLocaleString()}원</SDepositAmout>
           <SDepositSliderWrapper>
             <input
               type="range"
-              min={depositAmounts[0]}
-              max={depositAmounts[depositAmounts.length - 1]}
+              min={0}
+              max={depositAmounts.length - 1}
               step={1}
-              value={deposit}
+              defaultValue={0}
               onChange={(e) => {
-                handleChange(e.target.valueAsNumber);
+                setDeposit(depositAmounts[e.target.valueAsNumber]);
               }}
             />
           </SDepositSliderWrapper>
-          <SDepositBtnWrapper>
-            {depositAmounts.map((amount) => (
-              <li key={amount}>
-                <SDepositBtn
-                  isSelectedDeposit={amount === deposit}
-                  value={amount}
-                  type="button"
-                  onClick={() => setDeposit(amount)}
-                >
-                  <p>
-                    {amount.toLocaleString()}
-                    <span>원</span>
-                  </p>
-                </SDepositBtn>
-              </li>
-            ))}
-          </SDepositBtnWrapper>
         </SDepositSettingWrapper>
         <DepositRefundGuide />
         <SPaymentMethodWrapper>
@@ -91,7 +80,7 @@ export default function ChallengeParticipant() {
         <BottomFixedBtn
           btns={[
             {
-              text: `예치금 ${deposit.toLocaleString()}원 추가하여 신청`,
+              text: '신청하기',
               styleType: 'primary',
               size: 'large',
             },
@@ -104,15 +93,8 @@ export default function ChallengeParticipant() {
 
 const SChallengeParticipantWrapper = styled.div``;
 
-const SChallengeSummary = styled.section`
-  height: 84px;
-  background-color: lightblue;
-  text-align: center;
-`;
-
 const SDepositSettingWrapper = styled.section`
   border-bottom: 6px solid ${({ theme }) => theme.color.gray_ec};
-  height: 188px;
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
@@ -128,7 +110,7 @@ const SDepositAmout = styled.p`
 const SDepositSliderWrapper = styled.div`
   height: 60px;
   line-height: 60px;
-  margin: 0 20px;
+  margin: 0 1.25rem 1rem 1.25rem;
   & input[type='range'] {
     -webkit-appearance: none;
     accent-color: ${({ theme }) => theme.color.normal};
@@ -142,66 +124,35 @@ const SDepositSliderWrapper = styled.div`
   & input[type='range']::-webkit-slider-thumb {
     -webkit-appearance: none;
     appearance: none;
-    width: 28px;
-    height: 28px;
-    background: ${({ theme }) => theme.color.normal};
+    width: 40px;
+    height: 20px;
+    border-radius: 40px;
+    background: ${({ theme }) => theme.color.normal} url('/icons/icon-list.svg')
+      no-repeat center center;
+    background-size: 13px 13px;
     cursor: pointer;
-    border-radius: 50%;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
   }
 
   & input[type='range']::-moz-range-thumb {
-    width: 28px;
-    height: 28px;
-    background: ${({ theme }) => theme.color.normal};
+    width: 40px;
+    height: 20px;
+    border-radius: 40px;
+    background: ${({ theme }) => theme.color.normal} url('/icons/icon-list.svg')
+      no-repeat center center;
+    background-size: 13px 13px;
     cursor: pointer;
-    border-radius: 50%;
     border: none;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
   }
 
   & input[type='range']::-ms-thumb {
-    width: 28px;
-    height: 28px;
-    background: ${({ theme }) => theme.color.normal};
+    width: 40px;
+    height: 20px;
+    border-radius: 40px;
+    background: ${({ theme }) => theme.color.normal} url('/icons/icon-list.svg')
+      no-repeat center center;
+    background-size: 13px 13px;
     cursor: pointer;
-    border-radius: 50%;
     border: none;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
-  }
-`;
-
-const SDepositBtnWrapper = styled.ul`
-  display: flex;
-  overflow-x: auto;
-  margin: 0 1.25rem 2.5rem 1.25rem;
-  gap: 14px;
-  -webkit-overflow-scrolling: touch;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-const SDepositBtn = styled.button<{ isSelectedDeposit: boolean }>`
-  height: 30px;
-  padding: 0.25rem 0.625rem;
-  border: 1px solid
-    ${({ theme, isSelectedDeposit }) =>
-      isSelectedDeposit ? theme.color.normal : theme.color.gray_f9};
-  border-radius: 32px;
-  font-size: ${({ theme }) => theme.fontSize.body2};
-  font-weight: ${({ theme }) => theme.fontWeight.body2};
-  color: ${({ theme, isSelectedDeposit }) =>
-    isSelectedDeposit ? theme.color.white : theme.color.gray_3c};
-  background-color: ${({ theme, isSelectedDeposit }) =>
-    isSelectedDeposit ? theme.color.normal : theme.color.gray_f9};
-
-  > p {
-    width: 100%;
-    display: flex;
-    flex-flow: row nowrap;
   }
 `;
 
