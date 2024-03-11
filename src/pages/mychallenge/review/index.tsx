831998360c24680cf125000f6cfd4b56332e0bf4
sryung <sryung1225@gmail.com>
2024-03-12 00:49:02 +0900

    [✨ feat] 후기 작성 페이지 스타일링 및 라우팅 설정 #23
    /mychallenge에서 challengeId 전달하고, 폼 전송 완료 시 스낵바 띄우는 작업 필요

--- src/pages/mychallenge/review/index.tsx
+++ src/pages/mychallenge/review/index.tsx
@@ -1,16 +1,42 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Layout from '@/components/common/Layout';
+import WriteLayout from '@/components/common/WriteLayout';
+import writeLayoutText from '@/constants/writeLayoutText';
+import TextArea from '@/components/common/TextArea';
 
 export default function PostReview() {
   const router = useRouter();
-  const { challengeId } = router.query;
-  const [reviewContent] = useState('');
+  // const { challengeId } = router.query;
+  const challengId = 1;
+  const { placeholder } = writeLayoutText['후기작성'];
+  const [text, setText] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`${challengId}의 리뷰 작성 완료! : ${text}`);
+    router
+      .push({
+        pathname: '/mychallenge',
+        query: {
+          postReviewSuccess: true,
+        },
+      })
+      .catch((error) => {
+        console.error('페이지 이동 실패', error);
+      });
+  };
 
   return (
-    <Layout headerText="후기 작성" title="후기 작성" noFooter>
-      {challengeId}
-      <textarea value={reviewContent} maxLength={300} />
+    <Layout
+      headerText="후기 작성"
+      title="후기 작성"
+      description="챌린지 후기를 작성하는 페이지입니다."
+      noFooter
+    >
+      <WriteLayout pageType="후기작성" handleSubmit={handleSubmit} text={text}>
+        <TextArea placeholder={placeholder} text={text} setText={setText} />
+      </WriteLayout>
     </Layout>
   );
 }
