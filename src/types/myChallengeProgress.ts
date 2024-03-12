3ab6991cb6d2d99e1caa4c0f1cfd4d8bfa58b760
sryung <sryung1225@gmail.com>
2024-03-12 19:21:12 +0900

    [⚙️ chore] 진행 중인 챌린지 데이터 props 타입 지정 #27

---
+++ src/types/myChallengeProgress.ts
@@ -0,0 +1,9 @@
+interface IMyChallengProgress {
+  myChallengeId: number;
+  challengeGroupId: number;
+  groupTitle: string;
+  thumbnail: string;
+  startDate: string;
+}
+
+export default IMyChallengProgress;
