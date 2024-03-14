b8c93f03d82dec5e6dbef9769c343f5c400c002d
yujin1228 <starng11@naver.com>
2024-03-14 22:59:23 +0900

    [⚙️ chore] 날짜 계산용 상수 적용 #20

--- src/utils/calculateDDay.ts
+++ src/utils/calculateDDay.ts
@@ -1,8 +1,10 @@
+import ONE_DAY from '@/constants/day';
+
 export default function calculateDDay(date: string): number {
   const today = new Date();
   const targetDate = new Date(date);
   const timeDiff = targetDate.getTime() - today.getTime();
-  const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+  const daysDiff = Math.ceil(timeDiff / ONE_DAY);
 
   return daysDiff;
 }
