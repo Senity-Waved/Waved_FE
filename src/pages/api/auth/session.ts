b67b8bf5011a07be07d9fde2c2881f98ed82b2f4
jiyoon <vywns9978@gmail.com>
2024-03-15 15:29:52 +0900

    [✅ test] 테스트용 로그인 API 수정 및 httponly 변경, 에러 처리 #34

--- src/pages/api/auth/session.ts
+++ src/pages/api/auth/session.ts
@@ -8,12 +8,14 @@ export interface IAuth {
   refreshToken: string;
 }
 
-export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+export default async function Session(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   if (req.method === 'GET') {
     try {
-      // 인증 API로부터 토큰 가져오기
       const response = await axios.get<IAuth>(
-        'http://localhost:3000/api/login',
+        'http://localhost:3000/api/test/login',
       );
       const { hasInfo, accessToken, refreshToken } = response.data;
 
@@ -21,7 +23,7 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
         serialize('accessToken', accessToken, {
           path: '/',
           maxAge: 60 * 60 * 24 * 7,
-          httpOnly: true,
+          httpOnly: false,
           secure: process.env.NODE_ENV === 'development',
         }),
         serialize('refreshToken', refreshToken, {
@@ -34,12 +36,10 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
 
       res.status(200).json({ hasInfo, message: 'Authentication successful' });
     } catch (error) {
-      // 에러 처리
       console.error('Error fetching tokens:', error);
       res.status(500).json({ message: 'Authentication failed' });
     }
   } else {
-    // GET 요청이 아닌 경우 처리
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
--- src/pages/api/test/login.ts
+++ src/pages/api/test/login.ts
@@ -6,23 +6,21 @@ type Data = {
   refreshToken?: string;
 };
 
-export default function handler(
+export default function LoginHandler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
   if (req.method === 'GET') {
-    // 로그인 성공 시 보낼 테스트 토큰 값
     const testAccessToken = 'testaccesstoken';
     const testRefreshToken = 'testrefreshtoken';
 
-    // 클라이언트에 성공 메시지와 토큰 전송
     res.status(200).json({
       hasInfo: true,
       accessToken: testAccessToken,
       refreshToken: testRefreshToken,
     });
   } else {
-    res.setHeader('Allow', ['POST']);
+    res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
