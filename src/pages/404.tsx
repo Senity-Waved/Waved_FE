72eeb5183ef9505bdd5f852cc892875a79f5182a
sryung <sryung1225@gmail.com>
2024-03-18 01:15:25 +0900

    [🎨 style] 404 페이지 스타일링 수정 #38

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,34 +1,46 @@
 import styled from '@emotion/styled';
 import Image from 'next/image';
 import Link from 'next/link';
-import Layout from '@/components/common/Layout';
+import Head from 'next/head';
+import { SLayoutWrapper } from '@/components/common/Layout';
 
 export default function Custom404() {
   return (
-    <Layout
-      noHeader
-      noFooter
-      title="404 에러"
-      description="해당 페이지를 찾을 수 없습니다."
-    >
-      <SCustom404>
-        <Image
-          src="/icons/icon-404-error.svg"
-          alt="404 에러"
-          width={170}
-          height={76}
+    <SCustom404Wrapper noHeader noFooter>
+      <Head>
+        <title>WAVED | 404 에러</title>
+        <meta name="description" content="해당 페이지를 찾을 수 없습니다." />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
-        <SMainText>해당 페이지를 찾을 수 없습니다.</SMainText>
-        <SSubText>
-          찾으려는 페이지의 주소가 정확한지
-          <br />
-          다시 한번 확인해 주세요.
-        </SSubText>
-        <SLink href="/">홈으로 이동</SLink>
-      </SCustom404>
-    </Layout>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="a11yHidden">WAVED</h1>
+      <main>
+        <SCustom404>
+          <Image
+            src="/icons/icon-404-error.svg"
+            alt="404 에러"
+            width={170}
+            height={76}
+          />
+          <SMainText>해당 페이지를 찾을 수 없습니다.</SMainText>
+          <SSubText>
+            찾으려는 페이지의 주소가 정확한지
+            <br />
+            다시 한번 확인해 주세요.
+          </SSubText>
+          <SLink href="/">홈으로 이동</SLink>
+        </SCustom404>
+      </main>
+    </SCustom404Wrapper>
   );
 }
+
+const SCustom404Wrapper = styled(SLayoutWrapper)`
+  display: flex;
+`;
 
 const SCustom404 = styled.section`
   display: flex;
@@ -37,6 +49,7 @@ const SCustom404 = styled.section`
   align-items: center;
   width: 100%;
   height: 100%;
+  margin: auto 0;
 `;
 
 const SMainText = styled.h3`
