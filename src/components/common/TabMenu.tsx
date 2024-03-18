08b30c66093430b07b3582c215e8aedf20aa3b8e
sryung <sryung1225@gmail.com>
2024-03-18 12:24:21 +0900

    [🎨 style] 임의로 sticky 상단 여백 대처를 위한 1px 이동

--- src/components/common/TabMenu.tsx
+++ src/components/common/TabMenu.tsx
@@ -78,7 +78,7 @@ const STab = styled.li<{ isActive: boolean }>`
 
 const STabMenu = styled.ul<{ positionTop?: number }>`
   position: sticky;
-  top: ${({ positionTop }) => `calc(${positionTop}px + 56px)`};
+  top: ${({ positionTop }) => `calc(${positionTop}px + 55px)`};
   display: flex;
   width: 100%;
   height: 48px;
