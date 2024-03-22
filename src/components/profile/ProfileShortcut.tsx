adee06b89b8239f3369ffeb235b43bd3c93e84ff
jiyoon <vywns9978@gmail.com>
2024-03-22 16:54:45 +0900

    [⚙️ chore] IProfileProps 인터페이스 생성 #49

--- src/components/profile/ProfileShortcut.tsx
+++ src/components/profile/ProfileShortcut.tsx
@@ -3,7 +3,12 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { JOB_TITLE_KR } from '@/constants/jobTitle';
-import { IProfileProps } from '@/pages/profile';
+import IProfile from '@/types/profile';
+
+interface IProfileProps {
+  isLogined: boolean;
+  profileInfo: IProfile;
+}
 
 export default function ProfileShortcut({
   isLogined,
