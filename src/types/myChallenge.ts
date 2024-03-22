4ab5567cf29667e1729aea1d3e2c5cb1fa3eb3c0
yujin1228 <starng11@naver.com>
2024-03-22 18:11:21 +0900

    [⚙️ chore] 마이챌린지 타입 속성 설명 주석제거 #46

--- src/types/myChallenge.ts
+++ src/types/myChallenge.ts
@@ -10,10 +10,10 @@ export type TMyChallengeInfo = {
   myChallengeId: number;
   startDate: string;
   successCount: number;
-  isRefundRequested?: boolean | null; //환급여부
-  isSuccessed?: boolean | null; //최종달성여부
-  verificationType: TVerificationType; //챌린지 인증타입
-  deposit: number; //예치금
+  isRefundRequested?: boolean | null;
+  isSuccessed?: boolean | null;
+  verificationType: TVerificationType;
+  deposit: number;
 };
 
 export type TMyChallengeStatus = 'PROGRESS' | 'WAITING' | 'COMPLETED';
