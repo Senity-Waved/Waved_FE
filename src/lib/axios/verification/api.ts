5445f657ec8a19eea480e741ffeb27b273618695
yujin1228 <starng11@naver.com>
2024-03-22 17:49:41 +0900

    [✨ feat] 인증하기 및 인증내역 api 코드작성  #46

--- src/lib/axios/verification/api.ts
+++ src/lib/axios/verification/api.ts
@@ -1,13 +1,26 @@
 import axiosInstance from '../instance';
 
 export const postMyVerificationApi = (groupId: string, formData: FormData) => {
-  return axiosInstance.post(`/api/v1/verify/${groupId}`, formData, {
+  return axiosInstance.post(`/verify/${groupId}`, formData, {
     headers: { 'Content-type': 'multipart/form-data' },
   });
 };
 
-export const getQuizApi = (groupId: string) => {
-  return axiosInstance.get(`/api/v1/verify/${groupId}`);
+export const postMyCommitVerifiactionApi = (groupId: number) => {
+  return axiosInstance.post(`/verify/${groupId}`);
 };
 
-export const getVerifications = () => {};
+export const getQuizApi = (groupId: string) => {
+  return axiosInstance.get(`/verify/${groupId}`);
+};
+
+export const getMyStampApi = (myChallengeId: number) => {
+  return axiosInstance.get(`/myChallenges/${myChallengeId}`);
+};
+
+export const getVerificationsApi = (groupId: string, date: string) => {
+  // date -> 2024-03-19 00:00:00.0
+  return axiosInstance.get(
+    `/challengeGroups/${groupId}/dates?verificationDate=${date}`,
+  );
+};
