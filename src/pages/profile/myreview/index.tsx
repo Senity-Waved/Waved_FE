ecb44f238e50207015d7381e4229edf8ecafd416
sryung <sryung1225@gmail.com>
2024-03-22 22:35:25 +0900

    [✅ test] 배포 테스트용 목업 데이터 수정

--- src/pages/profile/myreview/index.tsx
+++ src/pages/profile/myreview/index.tsx
@@ -11,13 +11,6 @@ import Modal from '@/components/modal/Modal';
 
 const getMyReviews: IMyReview[] = [
   {
-    id: '2634637',
-    challengeTitle: '프론트엔드 기술 면접 1기',
-    createdDate: '2024년 03월 10일',
-    context:
-      '피그마 플러그인 개발에 관심이 생겨서 찾아보게 되었는데,많은 도움이 되네요.',
-  },
-  {
     id: '43436',
     challengeTitle: '1일 1커밋',
     createdDate: '2024년 02월 22일',
