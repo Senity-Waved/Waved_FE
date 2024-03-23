70b1d8718751abbbcf0b2d8c7e26a372f826fb14
jiyoon <vywns9978@gmail.com>
2024-03-24 03:19:21 +0900

    [⚙️ chore] 서버용 axiosInstance 파일 수정#57
    - 쿠키에 새로 발급 받은 액세스 토큰 저장하는 코드 추가
    - nextjs api routes 변경

--- src/lib/axios/serverInstance.ts
+++ src/lib/axios/serverInstance.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError } from 'axios';
-import { getCookie } from 'cookies-next';
+import { getCookie, setCookie } from 'cookies-next';
 import { GetServerSidePropsContext } from 'next';
 import { CustomAxiosRequestConfig } from './axiosConfig';
 
@@ -10,7 +10,7 @@ interface IReissueResponse {
 
 const createServerInstance = (context: GetServerSidePropsContext) => {
   const serverInstance = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1`,
+    baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true,
@@ -39,22 +39,29 @@ const createServerInstance = (context: GetServerSidePropsContext) => {
         try {
           const refreshToken = getCookie('refreshToken', { req: context.req });
           if (!refreshToken) {
-            throw new Error('재발급 요청을 위해 refreshToken 확인');
+            throw new Error(
+              'accessToken 재발급을 위한 refreshToken이 존재하지 않습니다.',
+            );
           }
 
           const { data: reissueData } = await axios.post<IReissueResponse>(
-            'http://127.0.0.1:3000/api/auth/reissue',
+            `${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}/api/auth/server/reissue`,
             { refreshToken },
             {
               headers: { 'Content-Type': 'application/json' },
             },
           );
 
-          console.log('새로 발급받은 액세스 토큰:', reissueData.accessToken);
+          console.log('✨ 액세스 토큰을 재발급받았습니다.');
 
-          if (customConfig.headers) {
-            customConfig.headers.Authorization = `Bearer ${reissueData.accessToken}`;
-          }
+          setCookie('accessToken', reissueData.accessToken, {
+            req: context.req,
+            res: context.res,
+            httpOnly: false,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 60 * 60 * 24 * 7,
+          });
+
           customConfig.isRetryRequest = true;
 
           return await serverInstance(customConfig);
