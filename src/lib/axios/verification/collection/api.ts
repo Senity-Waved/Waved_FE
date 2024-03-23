983246c3f34602d44b0fcbf121d96c6b34d9ccc8
yujin1228 <starng11@naver.com>
2024-03-24 04:30:58 +0900

    [✨ feat] 인증내역페이지에 필요한 api코드 작성 #64
    - 좋아요,좋아요취소,좋아요카운트get
    - 스탬프영역 정보 get
    - 날짜별 인증내역 api 요청 경로 수정

--- src/lib/axios/verification/collection/api.ts
+++ src/lib/axios/verification/collection/api.ts
@@ -1,12 +1,34 @@
+import IVerificationInfo from '@/types/verification';
 import axiosInstance from '../../instance';
 
-export const getMyStampApi = (myChallengeId: number) => {
-  return axiosInstance.get(`/myChallenges/${myChallengeId}`);
+export const getMyStampApi = async (
+  myChallengeId: number,
+): Promise<{ myVerifs: number[] }> => {
+  const response = await axiosInstance.get(`/myChallenges/${myChallengeId}`);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return response.data;
 };
 
-export const getVerificationsApi = (groupId: string, date: string) => {
-  // date -> 2024-03-19 00:00:00.0
-  return axiosInstance.get(
-    `/challengeGroups/${groupId}/dates?verificationDate=${date}`,
+export const getVerificationsApi = async (
+  groupId: string,
+  date: string,
+): Promise<IVerificationInfo[]> => {
+  // date -> 2024-03-19
+  const response = await axiosInstance.get(
+    `/challengeGroups/${groupId}?verificationDate=${date} 00:00:00.0`,
   );
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return response.data;
+};
+
+export const getLikeCountApi = (verificationId: number) => {
+  return axiosInstance.get(`/likes/${verificationId}`);
+};
+
+export const postLikeApi = (verificationId: number) => {
+  return axiosInstance.post(`/likes/${verificationId}`);
+};
+
+export const deleteLikeApi = (verificationId: number) => {
+  return axiosInstance.delete(`/likes/${verificationId}`);
 };
