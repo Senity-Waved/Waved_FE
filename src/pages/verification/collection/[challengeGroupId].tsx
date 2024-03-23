0e6daf9f8718799c27d506327339196c0f536baf
yujin1228 <starng11@naver.com>
2024-03-24 04:38:49 +0900

    [✨ feat] 목업데이터 삭제 후 api 연동 #64

--- src/pages/verification/collection/[challengeGroupId].tsx
+++ src/pages/verification/collection/[challengeGroupId].tsx
@@ -1,146 +1,107 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { GetServerSidePropsContext } from 'next';
 import styled from '@emotion/styled';
+import { RecoilEnv, useRecoilValue } from 'recoil';
 import ISnackBarState from '@/types/snackbar';
-import IVerificationInfo, { TVerificationType } from '@/types/verification';
 import ONE_DAY from '@/constants/day';
 import Layout from '@/components/common/Layout';
 import Stamp from '@/components/verification/collection/Stamp';
 import VerificationList from '@/components/verification/collection/VerificationList';
 import SnackBar from '@/components/common/SnackBar';
 import parseDate from '@/utils/parseDate';
+import ISelectedMyChallenge from '@/types/selectedMyChallenge';
+import ASelectedMyChallenge from '@/atoms/selectedMyChallenge';
+import IVerificationInfo from '@/types/verification';
+import {
+  getMyStampApi,
+  getVerificationsApi,
+} from '@/lib/axios/verification/collection/api';
 
-interface IVerificationCollection {
-  challengeTitle: string;
-  results: number[];
-  question?: string;
-  verifications: IVerificationInfo[];
-  startDate: string;
-  endDate: string;
-}
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
-const data: IVerificationCollection = {
-  challengeTitle: '기술 면접 챌린지 1기',
-  results: [1, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  question: '기술 면접 문제 내용입니다.',
-  startDate: '2024-03-10T00:00:00+09:00',
-  endDate: '2024-03-24T00:00:00+09:00',
-  verifications: [
-    {
-      verificationId: 1,
-      authorId: 1,
-      authorName: '내계정',
-      content:
-        '이 아티클에서는 일상 생활에 쉽게 통합할 수 있는 3가지 핵심 습관을 소개합니다. 첫 번째는 목표 설정과 시간 관리입니다. 이는 개인적 성취와 전문적 성장을 위한 기초를 마련합니다. 두 번째 습관은 긍정적 사고를 통한 자기 격려입니다. 이는 도전을 극복하고 성공으로 나아가는 데 중요합니다. 세 번째는 건강 유지를 위한 일상적인 운동과 균형 잡힌 식단입니다. 건강한 몸은 능률적인 마음의 기초입니다. 이러한 습관들은 개인의 성장과 발전에 필수적이며, 이 글을 통해 자기 계발의 길을 찾는 데 도움을 줄 것입니다.',
-      link: 'http://senity.com',
-      liked: true,
-      likeCount: 1,
-      time: '2024-03-12T00:00:05+09:00',
-    },
-    {
-      verificationId: 2,
-      authorId: 2,
-      authorName: '웨이브드11',
-      content:
-        '이 아티클에서는 여행자가 가져가야 할 4가지 필수 아이템을 상세히 소개합니다. 첫째, 편안한 여행을 위한 양질의 여행 가방. 두 번째는 다양한 환경에 대비할 수 있는 다용도 의류. 세 번째 아이템은 여행 중 긴급 상황에 대비한 응급 키트입니다. 네 번째는 휴대용 충전기와 보조 배터리로, 언제 어디서든 기기를 충전할 수 있게 해줍니다. 이러한 아이템들은 여행자가 만날 수 있는 다양한 상황에 대비할 수 있게 하며, 여행을 더욱 풍부하고 안전하게 만들어 줍니다. 여행을 떠나기 전 이 목록을 체크하고, 최고의 여행 경험을 준비하세요.',
-      liked: false,
-      likeCount: 2,
-      time: '2024-03-12T00:00:04+09:00',
-    },
-    {
-      verificationId: 3,
-      authorId: 3,
-      authorName: 'starng11df',
-      content:
-        '이 아티클에서는 여행자가 가져가야 할 4가지 필수 아이템을 상세히 소개합니다.',
-      liked: false,
-      likeCount: 3,
-      time: '2024-03-12T00:00:03+09:00',
-    },
-    {
-      verificationId: 4,
-      authorId: 4,
-      authorName: '여행가고싶다',
-      content:
-        '이 아티클에서는 여행자가 가져가야 할 4가지 필수 아이템을 상세히 소개합니다.',
-      liked: false,
-      likeCount: 4,
-      time: '2024-03-12T00:00:02+09:00',
-    },
-    {
-      verificationId: 5,
-      authorId: 5,
-      authorName: '테스트닉네임',
-      content: '답변은 최소 10글자 이상으로 확정',
-      liked: false,
-      likeCount: 5,
-      time: '2024-03-12T00:00:01+09:00',
-    },
-  ],
-};
-
-const data2: IVerificationCollection = {
-  challengeTitle: '스크린타임 4시간 챌린지 1기',
-  results: [1, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
-  question: '',
-  startDate: '2024-03-07',
-  endDate: '2024-03-21',
-  verifications: [
-    {
-      verificationId: 10,
-      authorId: 1,
-      authorName: '내계정',
-      content: '/images/image-waved-preview1.svg',
-      liked: true,
-      likeCount: 1,
-      time: '2024-03-12T00:00:03+09:00',
-    },
-    {
-      verificationId: 11,
-      authorId: 2,
-      authorName: '웨이브드2',
-      content: '/images/image-waved-preview2.svg',
-      liked: false,
-      likeCount: 2,
-      time: '2024-03-12T00:00:02+09:00',
-    },
-    {
-      verificationId: 12,
-      authorId: 3,
-      authorName: '웨이브드3',
-      content: 'https://via.placeholder.com/150x218.jpg',
-      liked: false,
-      likeCount: 3,
-      time: '2024-03-12T00:00:01+09:00',
-    },
-  ],
-};
-
-export default function VeirificationCollection({
-  challengeGroupId,
-  type,
-}: {
-  challengeGroupId: string;
-  type: TVerificationType;
-}) {
+export default function VeirificationCollection() {
   const router = useRouter();
   const { query } = useRouter();
+  const challengeGroupId = router.query.challengeGroupId as string;
   const [snackBarState, setSnackBarState] = useState<ISnackBarState>({
     open: false,
     text: '',
   });
 
+  const recoilMyChallengeData =
+    useRecoilValue<ISelectedMyChallenge>(ASelectedMyChallenge);
+  const [challengeData, setChallengeData] = useState<ISelectedMyChallenge>({
+    challengeGroupId: 0,
+    myChallengeId: 0,
+    groupTitle: '',
+    startDate: '',
+    endDate: '',
+    verificationType: 'TEXT',
+    status: 'PROGRESS',
+  });
+
+  const [stampData, setStampData] = useState<number[]>([]);
+  const [verificationsData, setVerificationsData] = useState<
+    IVerificationInfo[]
+  >([]);
+  const [isEmptyData, setIsEmptyData] = useState<boolean>(false);
+
   const today = new Date().getTime();
   const [date, setDate] = useState<number>(today);
   const [year, month, day] = parseDate(date);
   const isToday = parseDate(today).join('-') === `${year}-${month}-${day}`;
-  const isStartday = `${year}-${month}-${day}` === data.startDate.split('T')[0];
-
+  const isStartday =
+    `${year}-${month}-${day}` === challengeData.startDate.split('T')[0];
+  const isEndday =
+    `${year}-${month}-${day}` === challengeData.endDate.split('T')[0];
   const getNextDay = () => setDate(date + ONE_DAY);
   const getPreviousDay = () => setDate(date - ONE_DAY);
 
-  console.log(type, challengeGroupId);
+  // 날짜 기본 값 세팅
+  useEffect(() => {
+    const endDateTime = new Date(challengeData.endDate).getTime();
+    if (endDateTime < date) setDate(endDateTime);
+  }, [challengeData.endDate, date]);
+
+  // 리코일에서 마이챌린지 정보 가져오기
+  useEffect(() => {
+    setChallengeData(recoilMyChallengeData);
+    if (challengeData.myChallengeId) {
+      getMyStampApi(challengeData.myChallengeId)
+        .then((data) => {
+          setStampData(data.myVerifs);
+        })
+        .catch((error) => {
+          console.error(`getMyStampApi API 실패`, error);
+        });
+    }
+    if (challengeGroupId && date) {
+      getVerificationsApi(challengeGroupId, `${year}-${month}-${day}`)
+        .then((data) => {
+          setVerificationsData(data);
+        })
+        .catch((error) => {
+          if (
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            error.response.data === '해당 날짜에 존재하는 인증내역이 없습니다.'
+          ) {
+            setIsEmptyData(true);
+          }
+          console.error(`getVerificationsApi API 실패`, error);
+        });
+    }
+  }, [
+    recoilMyChallengeData,
+    challengeData.myChallengeId,
+    challengeGroupId,
+    year,
+    month,
+    day,
+    date,
+  ]);
+
+  console.log(verificationsData);
+  // 스낵바
   useEffect(() => {
     const handleRouting = (
       snackBarText: string,
@@ -155,9 +116,6 @@ export default function VeirificationCollection({
         .replace(
           {
             pathname: `/verification/collection/${challengeGroupId}`,
-            query: {
-              type,
-            },
           },
           undefined,
           {
@@ -177,20 +135,20 @@ export default function VeirificationCollection({
     if (query.submitVerification) {
       handleRouting('인증 제출이 완료되었습니다.');
     }
-  }, [query, router, challengeGroupId, type]);
+  }, [query, router, challengeGroupId]);
 
   return (
     <Layout
-      headerText={data.challengeTitle}
-      title={`인증내역-${data.challengeTitle}`}
+      headerText={challengeData.groupTitle}
+      title={`인증내역-${challengeData.groupTitle}`}
       description="챌린지의 인증내역을 확인하는 페이지입니다."
       noFooter
     >
       <SStampWrapper>
         <STitle>📌 내 인증 현황 </STitle>
-        <Stamp results={data.results} startDate={data.startDate} />
+        <Stamp results={stampData} startDate={challengeData.startDate} />
       </SStampWrapper>
-      {type !== 'GITHUB' && (
+      {challengeData.verificationType !== 'GITHUB' && (
         <>
           <SDateWrapper>
             <SDateBtn
@@ -204,16 +162,16 @@ export default function VeirificationCollection({
             <SDateBtn
               direction="next"
               onClick={getNextDay}
-              disabled={isToday}
+              disabled={isToday || isEndday}
             />
           </SDateWrapper>
           <VerificationList
-            verificationType={type}
-            verifications={
-              type === 'PICTURE' ? data2.verifications : data.verifications
-            }
+            verificationType={challengeData.verificationType}
+            challengeGroupId={challengeGroupId}
+            verifications={verificationsData}
+            date={`${year}-${month}-${day}`}
             isToday={isToday}
-            question={data.question}
+            isEmptyData={isEmptyData}
           />
         </>
       )}
@@ -227,23 +185,6 @@ export default function VeirificationCollection({
     </Layout>
   );
 }
-
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext,
-  // eslint-disable-next-line @typescript-eslint/require-await
-) => {
-  const { challengeGroupId, type } = context.query as {
-    challengeGroupId: string;
-    type: TVerificationType;
-  };
-
-  return {
-    props: {
-      challengeGroupId,
-      type,
-    },
-  };
-};
 
 const SStampWrapper = styled.section`
   padding: 1rem 1.25rem 0 1.25rem;
