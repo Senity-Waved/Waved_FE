596a48f9289a5014979bce896d86adb9a3fdaea1
sryung <sryung1225@gmail.com>
2024-03-25 02:36:11 +0900

    [✨ feat] 후기 수정 API 연동 #65
    - query를 이용해 이전 데이터 내용 전달 시, 새로고침하면 as 마스킹으로 인해 값이 사라지는 문제 존재. api get하는 방식으로 변경된 부분 적용
    - 기존에 작성된 후기 내용 데이터를 client 단에서 호출할 경우 전달 속도가 늦어져 server에서 전송하는 것이 빨라 SSR 렌더링 채택

--- src/lib/axios/review/api.ts
+++ src/lib/axios/review/api.ts
@@ -1,10 +1,11 @@
+import { AxiosInstance } from 'axios';
 import { IMyReviewList } from '@/types/review';
 import axiosInstance from '../instance';
 
 /**
  * 후기 작성 POST
- * @param myChallengeId
- * @returns response.data
+ * @param myChallengeId, content
+ * @returns message (성공: '리뷰를 작성했습니다')
  */
 const postReviewApi = (myChallengeId: string, content: string) => {
   return axiosInstance.post(`/reviews/myChallenge/${myChallengeId}`, content);
@@ -12,6 +13,7 @@ const postReviewApi = (myChallengeId: string, content: string) => {
 
 /**
  * 내가 작성한 후기 목록 조회 GET
+ * @param pageParam
  * @returns response.data
  */
 const getMyReviewsApi = (pageParam: number) => {
@@ -25,8 +27,11 @@ const getMyReviewsApi = (pageParam: number) => {
  * @param reviewId
  * @returns content
  */
-const getPrevReviewContentApi = (reviewId: number) => {
-  return axiosInstance.get<IMyReviewList[]>(`/reviews/${reviewId}`);
+const getPreviousReviewContentApi = (
+  serverInstance: AxiosInstance,
+  reviewId: string,
+) => {
+  return serverInstance.get<string>(`/reviews/${reviewId}`);
 };
 
 /**
@@ -34,7 +39,7 @@ const getPrevReviewContentApi = (reviewId: number) => {
  * @param reviewId, content
  * @returns message (성공: '리뷰를 수정했습니다')
  */
-const patchReviewApi = (reviewId: number, content: string) => {
+const patchReviewApi = (reviewId: string, content: string) => {
   return axiosInstance.patch(`/reviews/${reviewId}`, content);
 };
 
@@ -50,7 +55,7 @@ const deleteReviewApi = (reviewId: number) => {
 export {
   postReviewApi,
   getMyReviewsApi,
-  getPrevReviewContentApi,
+  getPreviousReviewContentApi,
   patchReviewApi,
   deleteReviewApi,
 };
