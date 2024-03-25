c1e127c32c2207a0dc3ed3d4ae0309c4fbbf4dc6
yujin1228 <starng11@naver.com>
2024-03-25 13:01:03 +0900

    [⚙️ chore] api 코드정리 및 주석추가 #64

--- src/lib/axios/verification/collection/api.ts
+++ src/lib/axios/verification/collection/api.ts
@@ -1,7 +1,17 @@
-import { AxiosError } from 'axios';
 import { IVerificationInfo } from '@/types/verification';
 import axiosInstance from '../../instance';
 
+/**
+ * 인증내역 페이지 정보 GET
+ * @param myChallengeId
+ * @returns
+ * {
+ *  myVerifys: [0,1,2]
+ *  groupTitle: "백엔드 기술면접 챌린지 2기",
+ *  startDate: "2024-03-11T00:00:00+09:00",
+ *  endDate: "2024-03-24T00:00:00+09:00"
+ * }
+ */
 export const getCollectionInfoApi = async (
   myChallengeId: string,
 ): Promise<{
@@ -15,104 +25,92 @@ export const getCollectionInfoApi = async (
   return response.data;
 };
 
+/**
+ * 날짜별 내 인증 내역 GET
+ * @param myChallengeId
+ * @param date
+ * @returns response.data -> 내역이 담겨있는 배열
+ */
 export const getMyVerifiactionApi = async (
   groupId: string,
-  date: string,
+  date: string, // -> 2024-03-19
 ): Promise<IVerificationInfo[]> => {
-  try {
-    const response = await axiosInstance.get(
-      `/challengeGroups/${groupId}/myVerifies?verificationDate=${date} 00:00:00.000`,
-    );
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return response.data;
-  } catch (error) {
-    const err = error as AxiosError;
-    if (err.code === 'ECONNABORTED') {
-      throw new Error('🚨 요청시간초과');
-    } else if (
-      err.response &&
-      err.response.data === '해당 날짜에 존재하는 인증내역이 없습니다.'
-    ) {
-      throw new Error('emptyData');
-    }
-    throw error;
-  }
+  const response = await axiosInstance.get(
+    `/challengeGroups/${groupId}/myVerifies?verificationDate=${date} 00:00:00.000`,
+  );
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return response.data;
 };
 
+/**
+ * 날짜별 전체사용자 인증 내역 GET
+ * @param myChallengeId
+ * @param date
+ * @returns response.data -> 내역이 담겨있는 배열
+ */
 export const getVerificationsApi = async (
   groupId: string,
   date: string, // -> 2024-03-19
 ): Promise<IVerificationInfo[]> => {
-  try {
-    const response = await axiosInstance.get(
-      `/challengeGroups/${groupId}?verificationDate=${date} 00:00:00.0`,
-    );
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return response.data;
-  } catch (error) {
-    const err = error as AxiosError;
-    if (err.code === 'ECONNABORTED') {
-      throw new Error('🚨 요청시간초과');
-    }
-    throw error;
-  }
+  const response = await axiosInstance.get(
+    `/challengeGroups/${groupId}?verificationDate=${date} 00:00:00.0`,
+  );
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return response.data;
 };
 
+/**
+ * 좋아요 개수 GET
+ * @param verificationId
+ * @returns
+ * {
+ *  verificationId: 1,
+ *  likedCount: 0
+ * }
+ */
 export const getLikeCountApi = async (
   verificationId: number,
 ): Promise<{ verificationId: number; likedCount: number }> => {
-  try {
-    const response = await axiosInstance.get(`/likes/${verificationId}`);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return response.data;
-  } catch (error) {
-    const err = error as AxiosError;
-    if (err.code === 'ECONNABORTED') {
-      throw new Error('🚨 요청시간초과');
-    }
-    throw error;
-  }
+  const response = await axiosInstance.get(`/likes/${verificationId}`);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return response.data;
 };
 
+/**
+ * 좋아요 POST
+ * @param verificationId
+ * @returns response.data
+ */
 export const postLikeApi = (verificationId: number) => {
-  try {
-    return axiosInstance.post(`/likes/${verificationId}`);
-  } catch (error) {
-    const err = error as AxiosError;
-    if (err.code === 'ECONNABORTED') {
-      throw new Error('🚨 요청시간초과');
-    }
-    throw error;
-  }
+  return axiosInstance.post(`/likes/${verificationId}`);
 };
 
+/**
+ * 좋아요 DELETE
+ * @param verificationId
+ * @returns response.data
+ */
 export const deleteLikeApi = (verificationId: number) => {
-  try {
-    return axiosInstance.delete(`/likes/${verificationId}`);
-  } catch (error) {
-    const err = error as AxiosError;
-    if (err.code === 'ECONNABORTED') {
-      throw new Error('🚨 요청시간초과');
-    }
-    throw error;
-  }
+  return axiosInstance.delete(`/likes/${verificationId}`);
 };
 
+/**
+ * 날짜별 기술면접 챌린지 퀴즈 GET
+ * @param challengeGroupId
+ * @param date
+ * @returns
+ * {
+ *  date: "2024-03-24T00:00:00+09:00",
+ *  question: "3월 24일 백엔드 퀴즈 문제"
+ * }
+ */
 export const getQuizApi = async (
   groupId: string,
   date: string,
 ): Promise<{ date: string; question: string }> => {
-  try {
-    const response = await axiosInstance.get(
-      `/verify/${groupId}/dates?quizDate=${date} 00:00:00.0`,
-    );
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return response.data;
-  } catch (error) {
-    const err = error as AxiosError;
-    if (err.code === 'ECONNABORTED') {
-      throw new Error('🚨 요청시간초과');
-    }
-    throw error;
-  }
+  const response = await axiosInstance.get(
+    `/verify/${groupId}/dates?quizDate=${date} 00:00:00.0`,
+  );
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return response.data;
 };
