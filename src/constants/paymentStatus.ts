68bbd2a27d6d2c98cdf49d1a83a1cd27e0d08cf4
jiyoon <vywns9978@gmail.com>
2024-03-26 17:53:42 +0900

    [⚙️ chore] 불필요한 예치금 관련 타입 및 파일 삭제 #67

--- src/constants/paymentStatus.ts
+++
@@ -1,8 +0,0 @@
-const paymentStatus = {
-  APPLIED: '신청',
-  SUCCESS: '성공',
-  CANCELED: '신청 취소',
-  FAIL: '실패',
-};
-
-export default paymentStatus;
--- src/types/paymentRecord.ts
+++
@@ -1,8 +0,0 @@
-import { PaymentStatusKey } from '@/components/profile/DepositItem';
-
-export default interface IPaymentRecord {
-  groupTitle: string;
-  status: PaymentStatusKey;
-  deposit: number;
-  createDate: string;
-}
