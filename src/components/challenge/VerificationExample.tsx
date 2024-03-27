770de2e2d8a138bdf1a6eab992065ee525bf48e1
sryung <sryung1225@gmail.com>
2024-03-27 11:21:51 +0900

    [✨ feat] 인증 방식 예시 이미지 없는 경우 섹션 미노출

--- src/components/challenge/VerificationExample.tsx
+++ src/components/challenge/VerificationExample.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { v4 as uuidv4 } from 'uuid';
 import styled from '@emotion/styled';
 import ScrollXBox from '@/components/common/ScrollXBox';
+import { SSectionTitle } from '@/pages/challenge/[groupId]';
 
 const getChallengeExamplePath = (title: string): string[] => {
   const titleArr = title.split(' ');
@@ -45,40 +46,39 @@ const getChallengeExamplePath = (title: string): string[] => {
       examplePathArr.push(...paths);
       break;
     }
-    default: {
-      const paths = [
-        'https://via.placeholder.com/300/E9F1FF/838A94.jpg?text=EXAMPLE',
-        'https://via.placeholder.com/300/E9F1FF/838A94.jpg?text=EXAMPLE',
-        'https://via.placeholder.com/300/E9F1FF/838A94.jpg?text=EXAMPLE',
-      ];
-      examplePathArr.push(...paths);
-    }
+    default:
   }
   return examplePathArr;
 };
 
 export default function VeirificationExample({ title }: { title: string }) {
+  const examplePaths = getChallengeExamplePath(title);
   return (
-    <ScrollXBox>
-      <SVerificationExample>
-        {getChallengeExamplePath(title).map((url) => (
-          <Image
-            key={uuidv4()}
-            src={url}
-            width={150}
-            height={218}
-            priority
-            alt={`${title} 인증 예시`}
-          />
-        ))}
-      </SVerificationExample>
-    </ScrollXBox>
+    examplePaths.length === 0 || (
+      <>
+        <SSectionTitle>예시</SSectionTitle>
+        <ScrollXBox>
+          <SVerificationExample>
+            {getChallengeExamplePath(title).map((url) => (
+              <Image
+                key={uuidv4()}
+                src={url}
+                width={150}
+                height={218}
+                priority
+                alt={`${title} 인증 예시`}
+              />
+            ))}
+          </SVerificationExample>
+        </ScrollXBox>
+      </>
+    )
   );
 }
 
 const SVerificationExample = styled.div`
-  height: 254px;
-  padding: 1rem 1.25rem;
+  height: 238px;
+  padding: 1rem 1.25rem 0;
   img {
     display: inline-block;
     &:not(:last-child) {
