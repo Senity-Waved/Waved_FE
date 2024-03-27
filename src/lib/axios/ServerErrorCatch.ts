51860f0b514c0d0fffbda1ac354a2843b46101a0
yujin1228 <starng11@naver.com>
2024-03-27 16:45:38 +0900

    [✨ feat] server axios error catch 함수 생성 #77

---
+++ src/lib/axios/ServerErrorCatch.ts
@@ -0,0 +1,46 @@
+import { AxiosError } from 'axios';
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  PreviewData,
+} from 'next';
+import { ParsedUrlQuery } from 'querystring';
+
+export default function ServerErrorCatch(
+  getServerSidePropsFunction: GetServerSideProps,
+) {
+  return async (
+    context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>,
+  ) => {
+    try {
+      return await getServerSidePropsFunction(context);
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      const { code } = axiosError;
+      const status = axiosError.response?.status;
+
+      if (axiosError.isAxiosError && axiosError.response && status === 500) {
+        return {
+          props: {
+            requireSnackBar: true, // snackbar 띄워줄지 여부
+            errorMsg: '문제가 발생했어요. 잠시 후 다시 시도해주세요.', // snackbar 메시지 내용
+          },
+        };
+      }
+      if (code === 'ECONNABORTED' || status === 408) {
+        return {
+          props: {
+            requireSnackBar: true, // snackbar 띄워줄지 여부
+            errorMsg: '요청시간이 초과되었습니다. 다시 시도 해주세요.', // snackbar 메시지 내용
+          },
+        };
+      }
+      return {
+        props: {
+          requireSnackBar: false,
+          errorMsg: axiosError.message || '에러가 발생했습니다.',
+        },
+      };
+    }
+  };
+}
