8a3d929ae2e1b086c65ddb65929f26131c34063a
jiyoon <vywns9978@gmail.com>
2024-03-28 15:33:02 +0900

    [⚙️ chore] 결제 아톰 recoil-persist 코드 수정

--- src/atoms/paymentSucceessState.ts
+++ src/atoms/paymentSucceessState.ts
@@ -1,10 +1,7 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
-const { persistAtom } = recoilPersist({
-  key: 'recoil-persist',
-  storage: localStorage,
-});
+const { persistAtom } = recoilPersist();
 
 interface IPaymentSuccessInfo {
   imp_uid: string;
