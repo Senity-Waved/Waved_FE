0cf38d4ad01bc84a6be60f1e79c14531823c6201
jiyoon <vywns9978@gmail.com>
2024-03-28 19:43:25 +0900

    [⚙️ chore] 스낵바 조건문 추가

--- src/pages/challenge/participant/index.tsx
+++ src/pages/challenge/participant/index.tsx
@@ -22,6 +22,7 @@ RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 export default function ChallengeParticipant() {
   const router = useRouter();
+  const { query } = useRouter();
   const depositAmounts = [0, 5000, 10000, 20000, 25000, 30000, 50000, 100000];
   const [myChallengeId, setMyChallengeId] = useState<number>(0);
   const updateSelectedChallengeData = useSetRecoilState(ASelectedChallenge);
@@ -52,24 +53,26 @@ export default function ChallengeParticipant() {
   });
 
   useEffect(() => {
-    setSnackBarState({
-      open: true,
-      text: '결제 실패 | 잠시후 재시도 바람',
-      type: 'warning',
-    });
-    router
-      .replace('/challenge/particpant', undefined, { shallow: true })
-      .catch((error: Error) =>
-        console.error('쿼리스트링 제거 후 URL 변경 실패', error),
-      );
-
-    setTimeout(() => {
+    if (query.payFailure) {
       setSnackBarState({
-        open: false,
-        text: '',
+        open: true,
+        text: '결제 실패 | 잠시후 재시도 바람',
+        type: 'warning',
       });
-    }, 3500);
-  }, [router]);
+      router
+        .replace('/challenge/particpant', undefined, { shallow: true })
+        .catch((error: Error) =>
+          console.error('쿼리스트링 제거 후 URL 변경 실패', error),
+        );
+
+      setTimeout(() => {
+        setSnackBarState({
+          open: false,
+          text: '',
+        });
+      }, 3500);
+    }
+  }, [query.payFailure, router]);
 
   useEffect(() => {
     setChallengeData(recoilChallengeData);
