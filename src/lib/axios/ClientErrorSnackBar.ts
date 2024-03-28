112303481636c15abde3264499903cb07f52ec5a
yujin1228 <starng11@naver.com>
2024-03-28 12:28:17 +0900

    [✨ feat] 블랙리스트토큰,인증정보없음 에러 클라이언트 에러스낵바에 추가

--- src/lib/axios/ClientErrorSnackBar.ts
+++ src/lib/axios/ClientErrorSnackBar.ts
@@ -11,6 +11,7 @@ export default function ClientErrorSnackBar() {
   const handleError = (error: AxiosError) => {
     const { code } = error;
     const status = error.response?.status;
+    const message = error.response?.data;
 
     if (code === 'ECONNABORTED' || status === 408) {
       openSnackBar('요청 시간이 초과되었습니다. 다시 시도 해주세요.');
@@ -18,6 +19,14 @@ export default function ClientErrorSnackBar() {
 
     if (code === 'ERR_NETWORK') {
       openSnackBar('네트워크 에러가 발생했습니다.');
+    }
+
+    if (message === '블랙리스트에 포함된 토큰입니다.') {
+      openSnackBar('블랙리스트에 포함된 토큰입니다.');
+    }
+
+    if (message === '인증정보가 없습니다.') {
+      openSnackBar('인증정보가 없습니다.');
     }
 
     if (status === 500) {
