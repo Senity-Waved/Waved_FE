6a2258f44546fce34bbbb3dd5dc2f1b9f3f9a141
sryung <sryung1225@gmail.com>
2024-03-28 20:39:03 +0900

    [✨ feat] 후기 작성 페이지 에러 핸들링 스낵바 처리 적용

--- src/pages/mychallenge/review/[myChallengeId].tsx
+++ src/pages/mychallenge/review/[myChallengeId].tsx
@@ -5,10 +5,13 @@ import WriteLayout from '@/components/common/WriteLayout';
 import writeLayoutText from '@/constants/writeLayoutText';
 import TextArea from '@/components/common/TextArea';
 import { postReviewApi } from '@/lib/axios/review/api';
+import useSnackBar from '@/hooks/useSnackBar';
+import SnackBar from '@/components/common/SnackBar';
 
 export default function PostReview() {
   const router = useRouter();
   const myChallengeId = router.query.myChallengeId as string;
+  const { snackBarData, openSnackBar } = useSnackBar();
   const { placeholder } = writeLayoutText['후기작성'];
   const [text, setText] = useState<string>('');
 
@@ -17,7 +20,7 @@ export default function PostReview() {
     postReviewApi(myChallengeId, text)
       .then((response) => {
         if (response) {
-          console.log(`${myChallengeId}의 리뷰 작성 완료! : ${text}`);
+          console.log(`${myChallengeId}의 후기 작성 완료! : ${text}`);
           router
             .push({
               pathname: '/mychallenge',
@@ -25,24 +28,30 @@ export default function PostReview() {
                 postReviewSuccess: true,
               },
             })
-            .catch((error) => console.error(error));
+            .catch((error) => {
+              console.error('페이지 에러 이동 실패', error);
+            });
         }
       })
-      .catch((error) =>
-        console.error(`${myChallengeId}의 리뷰 작성 실패`, error),
-      );
+      .catch((error) => {
+        console.error(`${myChallengeId}의 후기 작성 실패`, error);
+        openSnackBar('후기 작성을 실패했습니다.');
+      });
   };
 
   return (
     <Layout
       headerText="후기 작성"
       title="후기 작성"
-      description="챌린지 후기를 작성하는 페이지입니다."
+      description="챌린지 완료 후 후기를 작성하는 페이지입니다."
       noFooter
     >
       <WriteLayout pageType="후기작성" handleSubmit={handleSubmit} text={text}>
         <TextArea placeholder={placeholder} text={text} setText={setText} />
       </WriteLayout>
+      {snackBarData.open && (
+        <SnackBar text={snackBarData.text} type={snackBarData.type} noFooter />
+      )}
     </Layout>
   );
 }
