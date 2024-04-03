c85c7b5cae08700b0109f0fb6cb21bd95ee93d86
sryung <sryung1225@gmail.com>
2024-04-03 23:28:13 +0900

    [✨ feat] 인증 예시 이미지 크게보기 모달 추가 #89

--- src/components/challenge/VerificationExample.tsx
+++ src/components/challenge/VerificationExample.tsx
@@ -1,10 +1,18 @@
 import Image from 'next/image';
 import styled from '@emotion/styled';
-import ScrollXBox from '@/components/common/ScrollXBox';
+import React, { useState } from 'react';
 import { SSectionTitle } from '@/pages/challenge/[groupId]';
+import ScrollXBox from '@/components/common/ScrollXBox';
+import Portal from '@/components/modal/ModalPortal';
+import { SModalWrapper } from '@/components/modal/Modal';
 import getChallengeImagePath from '@/utils/getChallengeImagePath';
+import screenSize from '@/constants/screenSize';
 
 export default function VeirificationExample({ title }: { title: string }) {
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number>(-1);
+  const openModal = (index: number) => setSelectedImageIndex(index);
+  const closeModal = () => setSelectedImageIndex(-1);
+
   const examplePaths = getChallengeImagePath({
     title,
     type: 'verification',
@@ -16,15 +24,33 @@ export default function VeirificationExample({ title }: { title: string }) {
         <SSectionTitle>예시</SSectionTitle>
         <ScrollXBox>
           <SVerificationExample>
-            {examplePaths.map((url) => (
-              <Image
-                key={url}
-                src={url}
-                width={150}
-                height={218}
-                priority
-                alt={`${title} 인증 예시`}
-              />
+            {examplePaths.map((url, index) => (
+              <React.Fragment key={url}>
+                <SButton onClick={() => openModal(index)}>
+                  <Image
+                    src={url}
+                    width={150}
+                    height={218}
+                    priority
+                    alt={`${title} 인증 예시`}
+                  />
+                </SButton>
+                {selectedImageIndex === index && (
+                  <Portal>
+                    <SModalWrapper>
+                      <SPhotoModal>
+                        <SModalImage
+                          src={url}
+                          alt={`${title} 인증 예시 크게 보기`}
+                          fill
+                          sizes="100%"
+                        />
+                        <SCloseBtn type="button" onClick={closeModal} />
+                      </SPhotoModal>
+                    </SModalWrapper>
+                  </Portal>
+                )}
+              </React.Fragment>
             ))}
           </SVerificationExample>
         </ScrollXBox>
@@ -36,13 +62,39 @@ export default function VeirificationExample({ title }: { title: string }) {
 const SVerificationExample = styled.div`
   width: 100%;
   padding: 1rem 1.25rem 0;
+`;
+
+const SButton = styled.button`
+  &:not(:last-child) {
+    margin-right: 0.625rem;
+  }
+  &:last-child {
+    margin-right: 1.25rem;
+  }
   img {
     display: inline-block;
-    &:not(:last-child) {
-      margin-right: 0.625rem;
-    }
-    &:last-child {
-      margin-right: 1.25rem;
-    }
   }
+`;
+
+const SPhotoModal = styled.div`
+  position: relative;
+  aspect-ratio: 150/218;
+  min-width: calc(${screenSize.min}px - 4rem);
+  max-width: calc(${screenSize.max}px - 4rem);
+  width: calc(100% - 4rem);
+  border-radius: 8px;
+  overflow: hidden;
+`;
+
+const SModalImage = styled(Image)`
+  object-fit: cover;
+`;
+
+const SCloseBtn = styled.button`
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  width: 24px;
+  height: 24px;
+  background: url('/icons/icon-photo-delete.svg') no-repeat center;
 `;
