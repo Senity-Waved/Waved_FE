94ccda9ade36900066829504a2d114b74458bf19
jiyoon <vywns9978@gmail.com>
2024-04-08 15:08:35 +0900

    [⚙️ chore] clientErrorSnackBar 파일명 대소문자 변경 및 Recoil 환경 변수 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
+import { RecoilEnv, RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import theme from '@/styles/theme';
 import global from '@/styles/global';
-import ClientErrorSnackBar from '@/lib/axios/ClientErrorSnackBar';
+import ClientErrorSnackBar from '@/lib/axios/clientErrorSnackBar';
+
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
