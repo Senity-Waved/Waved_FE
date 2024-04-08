c215a34ba251ef762bc035609678efd164e8181c
jiyoon <vywns9978@gmail.com>
2024-04-08 16:31:48 +0900

    [✨ feat] 로그인 성공 시 리디렉션 되는 oauth 페이지에서 subscribe API 호출 #95
    - 현재 무조건 timeout 에러가 발생하고 있음.
    - 다만 api에서 돌려주는 리턴 값이 없어서 발생하는 것으로 추측이 됨.
    - 하지만 연재 subscribe가 성공했는지 여부를 확인할 수 있는 방법이 없어서 우선 subscribe는 후에 작업할 예정

--- src/pages/oauth/index.tsx
+++ src/pages/oauth/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { getCookie } from 'cookies-next';
 import LoadingSpinner from '@/components/common/LoadingSpinner';
+import { subscribeApi } from '@/lib/axios/notification/api';
 
 export default function Oauth() {
   const [accessToken, setAccessToken] = useState<string | null>('');
@@ -43,6 +44,13 @@ export default function Oauth() {
         .then((res) => {
           if (res.status === 200) {
             setIsTokenPosted(true);
+            subscribeApi()
+              .then((subscribeRes) => {
+                console.log('subscribe API 성공', subscribeRes);
+              })
+              .catch((error) => {
+                console.error('subscribe API 호출 실패 |', error);
+              });
           }
         })
         .catch((error) => {
