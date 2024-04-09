dede94addf040962f904fe811532cac494e10a7c
sryung <sryung1225@gmail.com>
2024-04-09 19:42:55 +0900

    [⏪ revert ]manifest.json 재연동 #100

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <link rel="manifest" href="/manifest.json" />
         <Script
           src="https://cdn.iamport.kr/v1/iamport.js"
           strategy="beforeInteractive"
