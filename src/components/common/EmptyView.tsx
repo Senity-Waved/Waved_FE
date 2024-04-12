d1449f6e3986b1f6049180717cb0e62e54b959fc
yujin1228 <starng11@naver.com>
2024-04-13 01:30:13 +0900

    [🔨 refactor] EmptyView 공통컴포넌트 페이지타입에 따라 다른 이미지 적용 #104

--- src/components/common/EmptyView.tsx
+++ src/components/common/EmptyView.tsx
@@ -1,35 +1,37 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import Image from 'next/image';
-import emptyText from '@/constants/emptyText';
+import emptyViewType from '@/constants/emptyViewType';
 
 interface IEmptyView {
   pageType:
     | '모집챌린지'
     | '마이챌린지'
+    | 'PROGRESS'
+    | 'WAITING'
+    | 'COMPLETED'
     | '챌린지후기'
     | '내후기'
     | '예치금내역'
     | '인증내역'
+    | '기술면접인증'
     | '커밋인증'
     | '알림내역';
+  center?: boolean;
 }
 
-export default function EmptyView({ pageType }: IEmptyView) {
-  const { mainText } = emptyText[pageType];
-  const { subText } = emptyText[pageType];
+export default function EmptyView({ pageType, center = true }: IEmptyView) {
+  const { mainText } = emptyViewType[pageType];
+  const { subText } = emptyViewType[pageType];
+  const { imagePath } = emptyViewType[pageType];
 
   return (
-    <SEmptyWrapper pageType={pageType}>
+    <SEmptyWrapper center={center} pageType={pageType}>
       <Image
-        src={
-          pageType === '커밋인증'
-            ? '/icons/icon-empty-github.svg'
-            : '/icons/icon-empty-character.svg'
-        }
+        src={imagePath}
         alt="데이터가 없을때 나오는 Empty view 화면 일러스트"
-        width={84}
-        height={84}
+        width={235}
+        height={215}
         priority
       />
       <SEmptyMainText>{mainText}</SEmptyMainText>
@@ -42,7 +44,7 @@ const centerStyle = css`
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -75%);
+  transform: translate(-50%, -50%);
 `;
 
 const SEmptyWrapper = styled.div<IEmptyView>`
@@ -51,22 +53,18 @@ const SEmptyWrapper = styled.div<IEmptyView>`
   flex-direction: column;
   align-items: center;
   gap: 0.5rem;
-  ${({ pageType }) =>
-    pageType === '마이챌린지' ||
-    pageType === '인증내역' ||
-    pageType === '커밋인증'
-      ? ''
-      : centerStyle}
+  ${({ center }) => (center ? centerStyle : '')}
 `;
 
 const SEmptyMainText = styled.h2`
   font-size: ${({ theme }) => theme.fontSize.headline2};
   font-weight: ${({ theme }) => theme.fontWeight.headline2};
-  color: ${({ theme }) => theme.color.gray_bf};
+  color: ${({ theme }) => theme.color.gray_3c};
+  margin-top: 0.5rem;
 `;
 
 const SEmptySubText = styled.p`
   font-size: ${({ theme }) => theme.fontSize.body4};
   font-weight: ${({ theme }) => theme.fontWeight.body4};
-  color: ${({ theme }) => theme.color.gray_bf};
+  color: ${({ theme }) => theme.color.gray_52};
 `;
--- src/constants/emptyViewType.ts
+++ src/constants/emptyViewType.ts
@@ -1,36 +1,64 @@
-const emptyText = {
+const emptyViewType = {
   모집챌린지: {
     mainText: '현재 모집중인 챌린지가 없어요',
     subText: '다음 챌린지가 모집될 때까지 잠시만 기다려주세요!',
+    imagePath: '/images/image-emptyview-mychallenge-progress.png',
   },
   마이챌린지: {
     mainText: '아직 내 챌린지가 없어요',
     subText: '챌린지를 신청하고 내 챌린지를 시작해보세요!',
+    imagePath: '/images/image-emptyview-mychallenge.png',
+  },
+  PROGRESS: {
+    mainText: '진행 중인 챌린지가 없어요.',
+    subText: '',
+    imagePath: '/images/image-emptyview-mychallenge-progress.png',
+  },
+  WAITING: {
+    mainText: '대기 중인 챌린지가 없어요.',
+    subText: '',
+    imagePath: '/images/image-emptyview-mychallenge-waiting.png',
+  },
+  COMPLETED: {
+    mainText: '진행을 완료한 챌린지가 없어요.',
+    subText: '',
+    imagePath: '/images/image-emptyview-mychallenge-completed.png',
   },
   챌린지후기: {
     mainText: '남기신 후기가 없어요',
     subText: '챌린지에 참여하시고 후기를 남겨보세요!',
+    imagePath: '/images/image-emptyview-review.png',
   },
   내후기: {
     mainText: '남기신 후기가 없어요',
     subText: '마이챌린지에서 참여하신 챌린지 후기를 남겨보세요!',
+    imagePath: '/images/image-emptyview-review.png',
   },
   예치금내역: {
     mainText: '예치금 내역이 없어요',
     subText: '예치금을 걸어서 챌린지에 참여해보세요!',
+    imagePath: '/images/image-emptyview-deposit.png',
   },
   인증내역: {
     mainText: '인증 내역이 없어요',
     subText: '아무도 인증을 하지 않은 날입니다.',
+    imagePath: '/images/image-emptyview-verification.png',
+  },
+  기술면접인증: {
+    mainText: '인증 후 인증 내역을 볼 수 있어요.',
+    subText: '오늘의 인증을 남겨보세요!',
+    imagePath: '/images/image-emptyview-verification.png',
   },
   커밋인증: {
     mainText: '나만의 커밋 인증',
     subText: '1일 1커밋 챌린지는 내 인증 내역만 볼 수 있습니다.',
+    imagePath: '/images/image-emptyview-commit.png',
   },
   알림내역: {
     mainText: '알림 목록이 비어 있어요',
     subText: '최근 2주 동안 받은 알림을 확인할 수 있어요.',
+    imagePath: '/images/image-emptyview-notification.png',
   },
 };
 
-export default emptyText;
+export default emptyViewType;
